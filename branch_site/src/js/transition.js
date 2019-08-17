2f58e87c0421c542b391763579fb053cf49006e6
Dmitry <piskulin.d95@mail.ru>
2019-08-17 12:58:03 +0300

    Добавил свой первый шаблон сайта

---
+++ branch_site/src/js/transition.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#footer_information_menu").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 800);
+    });
+    $("#nav_bar").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 800);
+    });
+    $("#nav_information_menu").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 800);
+    });
+});
